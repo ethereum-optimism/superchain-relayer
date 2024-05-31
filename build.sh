f548de299ee18333629804a662fb7546c6d85dfc
James Kim <jakim929@gmail.com>
2024-05-31 10:12:40 -0400

    chore: cleanup unused code

--- build.sh
+++
@@ -1,5 +0,0 @@
-PATH=$PATH:/opt/buildhome/.foundry/bin
-export PATH
-curl -L https://foundry.paradigm.xyz | bash
-foundryup
-npm run build
---
+++ src/envVars.ts
@@ -0,0 +1,14 @@
+import { inferSchemas, parseEnv } from 'znv'
+import { z } from 'zod'
+
+export const envVars = parseEnv(
+  import.meta.env,
+  inferSchemas({
+    VITE_MIXPANEL_TOKEN: {
+      schema: z.string(),
+    },
+    VITE_ENVIRONMENT: {
+      schema: z.enum(['development', 'production']),
+    },
+  }),
+)
