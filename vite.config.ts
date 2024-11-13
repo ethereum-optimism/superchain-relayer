acd5511656e397c174d81391eaac8991d78d4db8
James Kim <jakim929@gmail.com>
2024-11-13 15:46:06 -0800

    fix build and upgrade packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
-import path from 'path'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-/**
- * @see https://vitejs.dev/config/
- */
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
+  plugins: [react()],
   resolve: {
-    /**
-     * Polyfills nodejs imports
-     * @see https://vitejs.dev/config/shared-options.html#resolve-alias
-     */
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  /**
-   * Enables react
-   * @see https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md
-   */
-  plugins: [react()],
 })
